7150e4286d376391b5f89f076a472257f2eec1b5
EtoEto32 <c1204041@st.kanazawa-it.ac.jp>
2025-06-03 16:51:00 +0900

    main.pyにルーティングを追加

--- api/main.py
+++ api/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from api.routers import task, done
 
 app = FastAPI()
 
-@app.get("/hello")
-async def hello():
-    return {"message": "hello world"}
+app.include_router(task.router)
+app.include_router(done.router)
+
+@app.get("/")
+async def root():
+    return {"message": "Todo API"}
 
--- api/routers/task.py
+++ api/routers/task.py
@@ -1,17 +1,19 @@
 """
 ルーター定義のための初期化ファイル
 """ 
-from fastapi import APIRouter
+from fastapi import APIRouter,HTTPException
+from typing import Dict,List
+import api.schemas.tasks as task_schema
 
 router=APIRouter()
 
-@router.get("/tasks")
+@router.get("/tasks",response_model=List[task_schema.Task])
 async def list_tasks():
-    pass
+    return {"tasks": [task_schema.Task(id="1",title="1つ目のTODOタスク")]}
 
 @router.post("/tasks")
-async def create_tasks():
-    pass
+async def create_task():
+    return {"message": "Task created"}
 
 @router.put("/tasks/{task_id}")
 async def update_tasks():
