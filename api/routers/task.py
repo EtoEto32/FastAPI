30dba28865eda9c7fc3b83c6cf2ffb384eccf4a5
EtoEto32 <c1204041@st.kanazawa-it.ac.jp>
2025-06-10 12:27:32 +0900

    どわーw

--- api/routers/task.py
+++ api/routers/task.py
@@ -4,16 +4,18 @@
 from fastapi import APIRouter,HTTPException
 from typing import Dict,List
 import api.schemas.tasks as task_schema
+from api.schemas.tasks import *
 
 router=APIRouter()
 
-@router.get("/tasks",response_model=List[task_schema.Task])
+@router.get("/tasks",response_model=List[Task])
 async def list_tasks():
-    return {"tasks": [task_schema.Task(id="1",title="1つ目のTODOタスク")]}
+    return [Task(id=1, title="1つ目のTODOタスク", done=False)]
 
-@router.post("/tasks")
-async def create_task():
-    return {"message": "Task created"}
+@router.post("/tasks",response_model=task_schema.TaskCreateResponse)
+async def create_task(task_body:task_schema.TaskCreate):
+    return task_schema.TaskCreateResponse(id=1,**task_body.dict())
+
 
 @router.put("/tasks/{task_id}")
 async def update_tasks():
@@ -22,3 +24,4 @@ async def update_tasks():
 @router.delete("/tasks/{task_id}")
 async def delete_tasks():
     pass
+
